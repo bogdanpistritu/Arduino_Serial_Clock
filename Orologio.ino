7be5efd23254b5dce49c4679b8aa096771fc86e8
SiepeAssassina <lorenzo.tomasin@live.it>
2013-11-24 13:13:50 +0100

    BugFix
    Fixed some bugs and adapted to common anode

--- Orologio.ino
+++ Orologio.ino
@@ -15,7 +15,7 @@
 int SEG[8] = {2, 3, 4, 5, 6, 7, 8, 9}; 
 int Vp[4] = {A0, A1, A2, A3};
 boolean NMatrix[10][8] = {
-  {1, 1, 1, 1, 1, 0, 0, 0},  //0
+  {1, 1, 1, 1, 1, 1, 0, 0},  //0
   {0, 1, 1, 0, 0, 0, 0, 0},  //1
   {1, 1, 0, 1, 1, 0, 1, 0},  //2
   {1, 1, 1, 1, 0, 0, 1, 0},  //3
@@ -55,10 +55,14 @@ void update()
     for(int j = 0; j < 8; j++)
     {
       if(NMatrix[timeFigures[i]][j])
-        digitalWrite(SEG[j], HIGH);
-      else
         digitalWrite(SEG[j], LOW);
+      else
+        digitalWrite(SEG[j], HIGH);
     }  
+    delay(1);
     digitalWrite (Vp[i], LOW);
   }
 }
+
+
+
